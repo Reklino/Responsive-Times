fda79e7add90139661e43947f602efec94161fe9
Jacob Davidson <jdavidson@dg-adcenter021.rngint.net>
2015-09-03 17:10:20 -0500

    Initial poop

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp 			= require('gulp'),
+	autoprefixer 	= require('gulp-autoprefixer'),
+	sass			= require('gulp-sass'),
+	browserSync 	= require('browser-sync').create(),
+	reload      	= browserSync.reload,
+	svgstore		= require('gulp-svgstore'),
+	inject 			= require('gulp-inject');
+
+gulp.task('sass', function() {
+	return gulp.src('sass/*.scss')
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('css'))
+		.pipe(reload({stream: true}));
+})
+
+gulp.task('svgstore', function() {
+
+	var svgs = gulp
+       .src('svg/*.svg')
+       .pipe(svgstore({ inlineSvg: true }));
+
+   function fileContents (filePath, file) {
+       return file.contents.toString();
+   }
+
+   return gulp
+       .src('index.html')
+       .pipe(inject(svgs, { transform: fileContents }))
+       .pipe(gulp.dest('./'));
+
+})
+
+gulp.task('watch', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch('sass/*.scss', ['sass']);
+	gulp.watch('**/*.js').on('change', reload);
+	gulp.watch('*.html').on('change', reload);
+	gulp.watch('svg/*.svg', ['svgstore']).on('change', reload);
+})
+
+gulp.task('default', ['sass', 'svgstore', 'watch']);
